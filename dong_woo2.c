c528a405834063cf0a181f295dd1b6414975fa80
Red_Maple <namutip22@gmail.com>
2017-11-14 21:38:01 +0900

    Create dong_woo2.c

---
+++ dong_woo2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	int num = 0;
+	int direction, location;
+	char tile[10];
+	memset(tile,'_',sizeof(tile));
+	//printf("시작\n");
+	location = 5;
+	while(num<13){
+		direction = rand()%2;
+		if(direction == 0){
+			tile[location]='_';
+			location--;
+		}
+		else{
+			if(num == 0){
+				continue;
+			}
+			tile[location]='_';
+			location++;
+		}
+		tile[location] = '*';
+		for(int a=0; a<10; a++)
+			printf("%c",tile[a]);
+		printf("\n");
+		num++;
+	}
+	
+	
+	return 0;
+}
